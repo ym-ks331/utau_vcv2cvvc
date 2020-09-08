bf1d48c37970813f11cf4d9748102f2950cb0137
ks331 <orangecrow331@gmail.com>
2020-09-09 00:16:15 +0900

    リファクタ

--- vcv2cvvc.py
+++ vcv2cvvc.py
@@ -18,6 +18,9 @@ fh.setFormatter(formatter)
 LOG.addHandler(fh)
 
 
+class GetParamsError(Exception):
+    pass
+
 
 def main():
 
@@ -40,99 +43,101 @@ def main():
 
 def do_convert(args, oc):
 
-    with open(args.input, "r", encoding="sjis") as f:
-        with open("oto_cvvc.ini", "w", encoding="sjis") as f_out:
+    try:
+        with open(args.input, "r", encoding="sjis") as f:
+            with open("oto_cvvc.ini", "w", encoding="sjis") as f_out:
 
-            for line in f:
-                wavfile, params = line.split("=")
-                # エイリアス, オフセット, 子音部, ブランク, 先行発声, オーバーラップ
-                alias, offset, cons, cutoff, pre, ovl = params.split(",")
-                offset, cons, cutoff, pre, ovl = [float(val.strip()) for val in [offset, cons, cutoff, pre, ovl]]
+                for line in f:
+                    wavfile, params = line.split("=")
+                    # エイリアス, オフセット, 子音部, ブランク, 先行発声, オーバーラップ
+                    alias, offset, cons, cutoff, pre, ovl = params.split(",")
+                    offset, cons, cutoff, pre, ovl = [float(val.strip()) for val in [offset, cons, cutoff, pre, ovl]]
 
-                # 先頭の場合 -> そのまま出力する
-                if is_head(alias):
-                    f_out.write(line)
-                    continue
+                    # 先頭の場合 -> そのまま出力する
+                    if is_head(alias):
+                        f_out.write(line)
+                        continue
 
-                try:
-                    v,cv = alias.split(" ")
-                except ValueError:
-                # 分割できない場合 -> そのまま出力する
-                    f_out.write(line)
-                    continue
+                    try:
+                        v,cv = alias.split(" ")
+                    except ValueError:
+                    # 分割できない場合 -> そのまま出力する
+                        f_out.write(line)
+                        continue
 
-                # v-cvのc部分取り出す
-                c = oc.change_alias(cv)
+                    # v-cvのc部分取り出す
+                    c = oc.change_alias(cv)
 
-                if c is None:
-                    LOG.warning("変換できませんでした: {}".format(alias))
-                    f_out.write(line)
-                    continue
+                    if c is None:
+                        LOG.warning("変換できませんでした: {}".format(alias))
+                        f_out.write(line)
+                        continue
 
-                elif c == " ":
-                    vc = cv
+                    elif c == " ":
+                        vc = cv
 
-                else:
-                    vc="{} {}".format(v, c)
+                    else:
+                        vc="{} {}".format(v, c)
 
-                vcl = oc.get_vclength(c)
+                    try:
+                        vcl = oc.get_vclength(c)
+                    except(GetParamsError): 
+                        f_out.write(line)                    
+                        continue
 
-                if (vcl[1]==-1 and vcl[2] == -1):
-                    f_out.write(line)
+                    # 母音の場合 -> そのまま出力 + V
+                    if is_vowel(cv):
+                        f_out.write(line)
+                        
+                    else:
+                        # VC
+                        n_offset = round(offset - vcl[0])
+                        n_pre = round(pre)
+                        n_cons = n_pre
+                        n_cutoff = round(-(n_pre + vcl[1]))
+                        n_ovl = round(ovl)
+
+                        out = set_oto_line(wavfile,vc,n_offset,n_cons,n_cutoff,n_pre,n_ovl)
+                        f_out.write(out)
+
+                    cvl = oc.get_vclength(cv)
                     
-                    continue
+                    # CV
+                    n_offset = round(offset + pre - cvl[2])
+                    left = round(n_offset - offset)
+                    n_cons = round(cons-left) 
+                    migi = round(cutoff)
 
-                # 母音の場合 -> そのまま出力 + V
-                if is_vowel(cv):
-                    f_out.write(line)
+                    if migi < 0:
+                        n_cutoff = round(migi + left)
+                    else:
+                        n_cutoff = (cutoff) - left / 2
                     
-                else:
-                    # VC
-                    n_offset = round(offset - vcl[0])
-                    n_pre = round(pre)
-                    n_cons = n_pre
-                    n_cutoff = round(-(n_pre + vcl[1]))
-                    n_ovl = round(ovl)
-
-                    out = set_oto_line(wavfile,vc,n_offset,n_cons,n_cutoff,n_pre,n_ovl)
+                    n_pre = round(pre - left)
+                    n_ovl = round(cvl[3])
+                    
+                    out = set_oto_line(wavfile,cv,n_offset,n_cons,n_cutoff,n_pre,n_ovl)
                     f_out.write(out)
 
-                cvl = oc.get_vclength(cv)
-                
-                # CV
-                n_offset = round(offset + pre - cvl[2])
-                left = round(n_offset - offset)
-                n_cons = round(cons-left) 
-                migi = round(cutoff)
-
-                if migi < 0:
-                    n_cutoff = round(migi + left)
-                else:
-                    n_cutoff = (cutoff) - left / 2
-                
-                n_pre = round(pre - left)
-                n_ovl = round(cvl[3])
-                
-                out = set_oto_line(wavfile,cv,n_offset,n_cons,n_cutoff,n_pre,n_ovl)
-                f_out.write(out)
+    except(FileNotFoundError):
+        errmes="指定されたファイルが存在しません: {}".format(args.input)
+        print(errmes)
+        LOG.exception(errmes)
 
 
 def is_head(alias):
     '''
     語頭音素かどうかを判定する
     '''
-
     return alias.startswith("-")
 
 
 def is_vowel(str_):
-
     vowel = ["あ", "い", "う", "え", "お", "ん"]
     return str_ in vowel
 
 
 def set_oto_line(wavfile,alias,offset,cons,cutoff,pre,ovl):
-
     return  "{}={},{},{},{},{},{}\n".format(wavfile, alias, offset, cons, cutoff, pre, ovl)
 
 
@@ -170,8 +175,8 @@ class OtoConverter(object):
         
         if self.params.get(s):
             return  [self.params[s]["p1"], self.params[s]["p2"], self.params[s]["p3"], self.params[s]["p4"]]
-
-        return [-1, -1, -1, -1]
+        else:
+            raise GetParamsError("aaa")
 
 
 if __name__ == "__main__":
